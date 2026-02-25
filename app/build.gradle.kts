67397742a918dbafa0a6b483a42a620458f25d2f
helinertgrl <helinertgrll@gmail.com>
2026-02-25 16:12:58 +0300

    fix: add missing test dependencies to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,5 +71,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
 }
